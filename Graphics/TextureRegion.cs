7ada4269997d7f9267419192634b1810a84e9843
maxBRT <bourret74@gmail.com>
2026-01-05 14:48:12 -0500

    Add constructors to `TextureRegion` for initialization with texture and dimensions

--- Graphics/TextureRegion.cs
+++ Graphics/TextureRegion.cs
@@ -29,6 +29,24 @@ public class TextureRegion
     public int Height => SourceRectangle.Height;
 
     /// <summary>
+    /// Creates a new texture region.
+    /// </summary>
+    public TextureRegion() { }
+
+    /// <summary>
+    /// Creates a new texture region using the specified source texture.
+    /// </summary>
+    /// <param name="texture">The texture to use as the source texture for this texture region.</param>
+    /// <param name="x">The x-coordinate position of the upper-left corner of this texture region relative to the upper-left corner of the source texture.</param>
+    /// <param name="y">The y-coordinate position of the upper-left corner of this texture region relative to the upper-left corner of the source texture.</param>
+    /// <param name="width">The width, in pixels, of this texture region.</param>
+    /// <param name="height">The height, in pixels, of this texture region.</param>
+    public TextureRegion(Texture2D texture, int x, int y, int width, int height)
+    {
+        Texture = texture;
+        SourceRectangle = new Rectangle(x, y, width, height);
+    }
+    /// <summary>
     /// Submit this texture region for drawing in the current batch.
     /// </summary>
     /// <param name="spriteBatch">The spritebatch instance used for batching draw calls.</param>
